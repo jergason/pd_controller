f8350c6f5e44d81c84ec0bebe41bf6fb8525304d
Jamison Dance <jergason@gmail.com>
2011-10-02 14:18:30 -0600

    flag fields work

--- agent0.py
+++ agent0.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python -tt
-
-# An incredibly simple agent.  All we do is find the closest enemy tank, drive
-# towards it, and shoot.  Note that if friendly fire is allowed, you will very
-# often kill your own tanks with this code.
-
 #################################################################
-# NOTE TO STUDENTS
-# This is a starting point for you.  You will need to greatly
-# modify this code if you want to do anything useful.  But this
-# should help you to know how to interact with BZRC in order to
-# get the information you need.
-#
 # After starting the bzrflag server, this is one way to start
 # this code:
 # python agent0.py [hostname] [port]
@@ -30,64 +19,52 @@ from bzrc import *
 class Agent(object):
     """Class handles all command and control logic for a teams tanks."""
 
-    def __init__(self, bzrc):
+    def __init__(self, bzrc, color):
         self.bzrc = bzrc
+        self.color = color
         self.constants = self.bzrc.get_constants()
         self.commands = []
-        self.shoot_time_limit = random.uniform(1.5, 2.0)
-        self.state = "not turning"
-        self.last_shot_time = 0.0
-        self.move_time_limit = 4.0
-        self.last_turn_time = time.time() + self.move_time_limit
-        self.time_to_turn_60_degrees = 1.5
+        self.bases = self.bzrc.get_bases()
+        self.enemy = self.pick_enemy(self.color)
+        # How to tell what color I am?
+
+
+    def pick_enemy(self, own_color):
+        colors = ['red', 'blue', 'green', 'purple']
+        colors.remove(own_color)
+        return colors.[random.randint(0, 2)]
 
     def tick(self, time_diff):
         """Some time has passed; decide what to do next."""
-        #loop through each tank
-        #calculate field to goal
-        #move tank towards goal
         mytanks, othertanks, flags, shots = self.bzrc.get_lots_o_stuff()
-        #create tank objects
-        #calculate environment
-        #pass to react to get commands back from tanks
-        #then do commands
         self.mytanks = mytanks
         self.flags = flags
+        # We only care about our enemy's flag
+        enemy_flag = None
+        for flag in self.flags:
+            if flag.color == self.enemy:
+                enemy_flag = flag
 
+        # Flag is the goal, so it creates an attractive field
+        # Self.obstacles = self.bzrc.get_obstacles()
         self.commands = []
+        field = Field(flag.x, flag.y, 5, 500)
 
-        current_time = time.time()
-        # self.commands.append(SpeedCommand(self.mytanks[0].index, 1.0))
-        self.bzrc.speed(self.mytanks[0].index, 1.0)
-        # Check if we need to shoot
-        if current_time > self.last_shot_time + self.shoot_time_limit:
-            # self.commands.append(ShootCommand(self.mytanks[0].index))
-            print("shooting")
-            self.bzrc.shoot(self.mytanks[0].index)
-            self.last_shot_time = time.time()
-            self.shoot_time_limit = random.uniform(1.5, 2.0)
+        # Loop through each tank, calculating speed
+        for tank in self.mytanks:
+            self.bzrc.angvel(tank.index, self.calculate_angvel(tank, field))
+            self.bzrc.speed(tank.index, self.calculate_speed(tank, field))
 
-        # If we need to start turning
-        if current_time > self.last_turn_time + self.time_to_turn_60_degrees + self.move_time_limit and self.state != "turning":
-            self.last_turn_time = current_time
-            # self.commands.append(AngvelCommand(self.mytanks[0].index, 1))
-            print("turning")
-            self.bzrc.angvel(self.mytanks[0].index, 1.0)
-            self.state = "turning"
-        elif self.state == "turning":
-            # If we need to stop turning
-            if current_time > self.last_turn_time + self.time_to_turn_60_degrees:
-                self.bzrc.angvel(self.mytanks[0].index, 0.0)
-                print("not turning")
-                # self.commands.append(AngvelCommand(self.mytanks[0].index, 0))
-                self.state = "not_turning"
-
-
-
-        # for tank in mytanks:
-        #     self.attack_enemies(tank)
-
-        # results = self.bzrc.do_commands(self.commands)
+    def calculate_angvel(self, tank, fields):
+        res = field_calculator.calcuate_field_to_goal({'x': tank.x, 'y': tank.y}, {'x': fields.x, 'y': fields.y}, fields.r, fields.s)
+        # Res is a vector. now compare the angle of the vector to our angle.
+        target_angle_in_radians = math.atan2(res['x'], res['y'])
+        tank_angle = tank.angle
+        # Compare two angles. pick a direction for us to turn?
+        if tank_angle > target_angle_in_radians:
+            return -1
+        else:
+            return 1
 
     def attack_enemies(self, tank):
         """Find the closest enemy and chase it, shooting as you go."""
@@ -128,7 +105,7 @@ class Agent(object):
 def main():
     # Process CLI arguments.
     try:
-        execname, host, port = sys.argv
+        execname, host, port, color = sys.argv
     except ValueError:
         execname = sys.argv[0]
         print >>sys.stderr, '%s: incorrect number of arguments' % execname
@@ -139,7 +116,7 @@ def main():
     #bzrc = BZRC(host, int(port), debug=True)
     bzrc = BZRC(host, int(port))
 
-    agent = Agent(bzrc)
+    agent = Agent(bzrc, color)
 
     prev_time = time.time()
 
--- field_calculator.py
+++ field_calculator.py
@@ -3,14 +3,13 @@ import math
 #todo: how to calculate what my angular velocity should be
 # based on what these vectors are?
 
-def calculate_field_to_goal(xy, goal_xy, r):
+def calculate_field_to_goal(xy, goal_xy, r, s):
     alpha = 0.1
-    spread = 1000.0
     d = math.sqrt((xy['x'] - goal_xy['x']) ** 2 + (xy['y'] - goal_xy['y']) ** 2)
     theta = math.atan2((goal_xy['y'] - xy['y']) / (goal_xy['x'] - xy['x']))
     if d < r:
-        return [0, 0]
-    elif r <= d and d <= spread + r:
-        return [alpha * (d - r) * math.cos(theta), alpha * (d - r) * math.sin(theta)]
+        return {'x': 0, 'y': 0}
+    elif r <= d and d <= s + r:
+        return { 'x': alpha * (d - r) * math.cos(theta), 'y': alpha * (d - r) * math.sin(theta) }
     else:
-        return [alpha * spread * math.cos(theta), alpha * spread * math.sin(theta)]
+        return { 'x': alpha * s * math.cos(theta), 'y': alpha * s * math.sin(theta) }
